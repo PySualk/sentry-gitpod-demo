e66728414efa66e18b22a97b97aac367bebf9b86
Klaus Wi <github2@archmail.de>
2024-02-11 10:37:12 +0000

    add spring boot code

---
+++ backend/sentry-gitpod-demo/src/main/java/org/sualk/sentrygitpoddemo/SentryGitpodDemoApplication.java
@@ -0,0 +1,13 @@
+package org.sualk.sentrygitpoddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentryGitpodDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentryGitpodDemoApplication.class, args);
+	}
+
+}
---
+++ backend/sentry-gitpod-demo/src/test/java/org/sualk/sentrygitpoddemo/SentryGitpodDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sualk.sentrygitpoddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentryGitpodDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
