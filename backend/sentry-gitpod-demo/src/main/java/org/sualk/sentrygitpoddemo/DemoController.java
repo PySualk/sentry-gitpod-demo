193f62cc528d1126e612664f32570311f4c07f26
Klaus Wilhelmi <klaus.wilhelmi@detim.de>
2024-02-13 15:14:52 +0100

    update frontend

--- backend/sentry-gitpod-demo/src/main/java/org/sualk/sentrygitpoddemo/DemoController.java
+++ backend/sentry-gitpod-demo/src/main/java/org/sualk/sentrygitpoddemo/DemoController.java
@@ -1,28 +1,29 @@
 package org.sualk.sentrygitpoddemo;
 
-import java.util.List;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
+import io.sentry.Sentry;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.Exception;
-import io.sentry.Sentry;
+import java.util.List;
 
 @RestController
+@Slf4j
 class DemoController {
 
-    @GetMapping
-    public void get() {
-        System.out.println("foo");
+    @GetMapping("error")
+    public void error() {
+        log.info("GET /error");
         try {
-          throw new Exception("This is a test.");
+          throw new Exception("This is a generic error.");
         } catch (Exception e) {
           Sentry.captureException(e);
         }
+    }
+
+    @GetMapping("data")
+    public List<String> data() {
+        log.info("GET /data");
+        return List.of("apple", "banana", "orange");
     }
 }
--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,33 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button
-            type="button"
-            onClick={() => {
-              throw new Error("Sentry Test Error");
-            }}
-        >
-          Break the world
-        </button>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState(null);
+
+    function simulateError() {
+        throw new Error("Sentry Test Error");
+    }
+
+    function handleClick() {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', 'http://localhost:8080/data');
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                setData(JSON.parse(xhr.responseText));
+            }
+        };
+        xhr.send();
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <div>
+                    <button onClick={simulateError}>
+                        Simulate local error
+                    </button>
+                </div>
+                <div>
+                    <button onClick={handleClick}>Fetch data from backend (wrong url)</button>
+                    {data ? <div>{JSON.stringify(data)}</div> : <div>Loading...</div>}
+                </div>
+            </header>
+        </div>
+    )
+        ;
 }
 
 export default App;
